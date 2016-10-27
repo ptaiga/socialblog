c318209503689a8afab9912455b1e965ec010ced
Pavel Taiga <ptaiga@gmail.com>
2016-10-27 13:29:28 +0300

    Fixed initial project dependencies

--- socialblog/settings.py
+++ socialblog/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'socialblog1.urls'
+ROOT_URLCONF = 'socialblog.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'socialblog1.wsgi.application'
+WSGI_APPLICATION = 'socialblog.wsgi.application'
 
 
 # Database
