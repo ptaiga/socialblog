579ec04d6c683df58704be3dd33ebb8f5d844fc4
Pavel Taiga <ptaiga@gmail.com>
2016-10-28 13:48:03 +0300

    Updated viewing article instances in console

--- main/models.py
+++ main/models.py
@@ -4,3 +4,5 @@ class Article(models.Model):
     header = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     content = models.TextField()
+    def __str__(self):
+        return self.header
