620725d168987f86c85cbaae3a726527943fc2db
Pavel Taiga <ptaiga@gmail.com>
2016-11-02 11:18:43 +0300

    Modified index view

--- main/views.py
+++ main/views.py
@@ -1,15 +1,12 @@
+from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import RequestContext, loader
 
 from .models import Article
 
 def index(request):
     latest_post_list = Article.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('main/index.html')
-    context = RequestContext(request, {
-        'latest_post_list': latest_post_list,
-    })
-    return HttpResponse(template.render(context))
+    context = {'latest_post_list': latest_post_list}
+    return render(request, 'main/index.html', context)
 
 def post(request, post_id):
     return HttpResponse("You're looking at post %s." % post_id)
