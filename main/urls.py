9672a044fea4d1eacf11cfe64765a4a63d701530
Pavel Taiga <ptaiga@gmail.com>
2016-11-02 20:08:47 +0300

    Index show only login user article

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'main'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<post_id>[0-9]+)/$', views.post, name='post')
+    url(r'^(?P<post_id>[0-9]+)/$', views.post, name='post'),
+    url(r'^users/$', views.users, name='users'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 
+from django.contrib.auth.models import User
+
 from .models import Article
 
 def index(request):
-    latest_post_list = Article.objects.order_by('-pub_date')[:5]
+    post_list = Article.objects.filter(user=request.user) 
+    #.order_by('-pub_date')[:5]
     context = {
-        'latest_post_list': latest_post_list,
+        'post_list': post_list,
         'user': request.user,
     }
     return render(request, 'main/index.html', context)
@@ -14,3 +17,8 @@ def index(request):
 def post(request, post_id):
     article = get_object_or_404(Article, pk=post_id)
     return render(request, 'main/post.html', {'article': article})
+
+def users(request):
+    user_list = User.objects.all()
+    output = ', '.join([u.username + ': ' + str(u.pk) for u in user_list])
+    return HttpResponse(output)
