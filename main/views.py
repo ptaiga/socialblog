55a36bfa466f9bf5d980a833c8cde88254c866ab
Pavel Taiga <ptaiga@gmail.com>
2016-11-02 10:44:23 +0300

    Added plug views 'post'

--- main/views.py
+++ main/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Blogsocial index page")
+
+def post(request, post_id):
+    return HttpResponse("You're looking at post %s." % post_id)
