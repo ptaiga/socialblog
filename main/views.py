efe1b501d22d41e269189556c2263224c990b9e2
Pavel Taiga <ptaiga@gmail.com>
2016-11-02 12:25:56 +0300

    Added post detail view and template

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 
 from .models import Article
 
@@ -9,4 +9,5 @@ def index(request):
     return render(request, 'main/index.html', context)
 
 def post(request, post_id):
-    return HttpResponse("You're looking at post %s." % post_id)
+    article = get_object_or_404(Article, pk=post_id)
+    return render(request, 'main/post.html', {'article': article})
