d9c040cdb737c594308bb3a80a89a1bbef2e5f12
Pavel Taiga <ptaiga@gmail.com>
2016-11-02 10:53:07 +0300

    Added showing for header of articles options in index view

--- main/views.py
+++ main/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 from django.http import HttpResponse
-
+from .models import Article
 
 def index(request):
-    return HttpResponse("Blogsocial index page")
+    latest_post_list = Article.objects.order_by('-pub_date')[:5]
+    output = ', '.join([a.header for a in latest_post_list])
+    return HttpResponse(output)
+    #return HttpResponse("Blogsocial index page")
 
 def post(request, post_id):
     return HttpResponse("You're looking at post %s." % post_id)
