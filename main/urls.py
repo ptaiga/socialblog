2599f5be565655b3d339ede7a33f3f3af15fad6c
Pavel Taiga <ptaiga@gmail.com>
2016-11-02 12:37:13 +0300

    Removed url hardcore

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'main'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<post_id>[0-9]+)/$', views.post, name='post')
