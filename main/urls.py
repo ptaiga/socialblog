a5dd7f8c1b3dec209e147709bbe3f42d4664a1dd
Pavel Taiga <ptaiga@gmail.com>
2016-11-08 17:47:30 +0300

    Added the ability to create new article

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,7 @@ app_name = 'main'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<post_id>[0-9]+)/$', views.post, name='post'),
+    url(r'^post/new/$', views.new_post, name='new_post'),
+    url(r'^post/add/$', views.add_post, name='add_post'),
     url(r'^users/$', views.users, name='users'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.utils import timezone
 
 from django.contrib.auth.models import User
 
@@ -8,9 +10,7 @@ from .models import Article
 def index(request):
     subscribe_list = request.user.subscriber.subscribe_list.split(',')
     users = User.objects.filter(id__in = subscribe_list)
-    post_list = Article.objects.filter(user__in=users)
-    #post_list = Article.objects.filter(user=request.user) 
-    #.order_by('-pub_date')[:5]
+    post_list = Article.objects.filter(user__in=users).order_by('-pub_date')
     context = {
         'post_list': post_list,
         'user': request.user,
@@ -22,6 +22,21 @@ def index(request):
 def post(request, post_id):
     article = get_object_or_404(Article, pk=post_id)
     return render(request, 'main/post.html', {'article': article})
+
+def new_post(request):
+    return render(request, 'main/new_post.html', {})
+
+def add_post(request):
+    if (request.method == 'POST'):
+        a = Article(\
+            user=request.user,\
+            header=request.POST['header'],\
+            content=request.POST['content'],\
+            pub_date=timezone.now())
+        a.save()
+        return HttpResponseRedirect(reverse('main:index', args=()))
+    else:
+        return HttpResponse('Something went wrong')
 
 def users(request):
     user_list = User.objects.all()
