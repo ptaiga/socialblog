bdd850b944f5ba9b0438d5c89dcbc71b93594568
Pavel Taiga <ptaiga@gmail.com>
2016-11-08 16:12:48 +0300

    Index page show headers of article according subscribe

--- main/views.py
+++ main/views.py
@@ -6,11 +6,16 @@ from django.contrib.auth.models import User
 from .models import Article
 
 def index(request):
-    post_list = Article.objects.filter(user=request.user) 
+    subscribe_list = request.user.subscriber.subscribe_list.split(',')
+    users = User.objects.filter(id__in = subscribe_list)
+    post_list = Article.objects.filter(user__in=users)
+    #post_list = Article.objects.filter(user=request.user) 
     #.order_by('-pub_date')[:5]
     context = {
         'post_list': post_list,
         'user': request.user,
+        'subscribe_list': subscribe_list,
+        'users': users
     }
     return render(request, 'main/index.html', context)
 
@@ -20,5 +25,6 @@ def post(request, post_id):
 
 def users(request):
     user_list = User.objects.all()
-    output = ', '.join([u.username + ': ' + str(u.pk) for u in user_list])
-    return HttpResponse(output)
+    return render(request, 'main/users.html', {'user_list': user_list})
+    #output = ', '.join([u.username + ': ' + str(u.pk) for u in user_list])
+    #return HttpResponse(output)
